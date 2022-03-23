b9b029466f0f04106b3cc5bc8f7db47f03296d18
Alvaro <begninialvaro@gmail.com>
2022-03-23 16:04:24 -0400

    transpiled tests

---
+++ dist/tests/index.test.js
@@ -0,0 +1,33 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const index_1 = __importDefault(require("../src/index"));
+const bot = new index_1.default("5118466828:AAGpZmebzzwTg_luVxP9OBgi_PteNgRg_Wk", "1341716338");
+describe("Testing index.ts", () => {
+    test("Send a message in a telegram chat", () => __awaiter(void 0, void 0, void 0, function* () {
+        expect((yield bot.sendMessage({ message: "Hello world" })).ok).toBe(true);
+    }));
+    test("Send a contact in a telegram chat", () => __awaiter(void 0, void 0, void 0, function* () {
+        expect((yield bot.sendContact({ phoneNumber: "65999999999", firstName: "Alvaro" })).ok).toBe(true);
+    }));
+    test("Send a dice in a telegram chat", () => __awaiter(void 0, void 0, void 0, function* () {
+        expect((yield bot.sendDice()).ok).toBe(true);
+    }));
+    test("Send a poll in a telegram chat", () => __awaiter(void 0, void 0, void 0, function* () {
+        expect((yield bot.sendPoll({ question: "Some random text", options: ["1", "2", "3", "4"] })).ok).toBe(true);
+    }));
+    test("Get updates from bot", () => __awaiter(void 0, void 0, void 0, function* () {
+        expect((yield bot.getUpdates()).ok).toBe(true);
+    }));
+});
