da1e40bb99d7e254517fc413a8e14eaf957f340d
Alvaro <begninialvaro@gmail.com>
2022-03-23 08:32:46 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,107 @@
+# A Simple bot manager for telegram
+
+
+## **Getting started**
+#### you will need a bot created with botFather
+Create a bot using the botFather of telegram [here](https://t.me/botfather)\
+Copy the bot token and your chat id
+
+
+## Start your bot here:
+
+```typescript
+const bot = new TelegramBot("your token here", "chatId here");
+```
+The chatId is not mandatory but it's recommended to use.
+Otherwise you will have to send the *chatId* in every request
+
+
+## **Sending message**
+
+
+```typescript
+const bot = new TelegramBot("your token here", "chatId here");
+
+async function sendMessage() {
+  const response = bot.sendMessage({ message: "Hello world" });
+  console.log(response)
+}
+```
+
+## **Sending message to another chat**
+If you want to send a message to another chat you can just add *chatId* in your request
+
+```typescript
+const bot = new TelegramBot("your token here", "chatId here");
+
+async function sendMessage() {
+  const response = bot.sendMessage({ message: "Hello world", chatId: "your chatId here"});
+  console.log(response)
+}
+```
+
+## **Sending contacts**
+
+
+```typescript
+const bot = new TelegramBot("your token here", "chatId here");
+
+async function sendContact(){
+  const response = bot.sendContact({ phoneNumber: "+556599999999", firstName: "Alvaro"})
+  console.log(response)
+}
+```
+
+## **Get updates**
+
+```typescript
+const bot = new TelegramBot("your token here", "chatId here");
+
+async function getUpdates(){
+  const response = bot.getUpdates()
+  console.log(response)
+}
+```
+
+## **Send poll**
+
+
+
+```typescript
+const bot = new TelegramBot("your token here", "chatId here");
+
+async function sendPoll(){
+  const response = bot.sendPoll({question: "Some question here", options: ["option1", "option2", "option3"]})
+  console.log(response)
+}
+
+//By default the type of polling will be regular, but you can make a quiz with {type: "quiz"}
+```
+
+## **Silent notifications**
+The user receives the notifications but without any sound
+
+```typescript
+const bot = new TelegramBot("your token here", "chatId here");
+
+async function silentMessage(){
+  const response = bot.sendMessage({message: "Silence!", disableNotification: true})
+  console.log(response)
+}
+
+
+```
+
+
+## **Send dice**
+send a dice that lands on a random number
+
+
+```typescript
+const bot = new TelegramBot("your token here", "chatId here");
+
+async function silentMessage(){
+  const response = bot.sendDice()
+  console.log(response)
+}
+```
---
+++ src/index.ts
@@ -0,0 +1,97 @@
+import axios from "axios"
+import qs from "qs"
+
+
+type sendContact = {
+  chatId?: string,
+  phoneNumber: string,
+  firstName: string,
+  disableNotification?: boolean,
+}
+
+type sendPoll = {
+  question: string,
+  options: string[],
+  type?: string,
+  correctOptionID?: number,
+  chatId?: string,
+  disableNotification?: boolean,
+}
+
+type sendMessage = {
+  message: string,
+  chatId?: string
+  disableNotification?: boolean,
+}
+
+
+export default class TelegramBot {
+  token: string
+  chatId?: string
+  path: string
+
+  constructor(botToken: string, chatId: string) {
+    this.token = botToken
+    this.chatId = chatId
+    this.path = `https://api.telegram.org/bot${this.token}`
+  }
+
+  async sendContact({ phoneNumber, firstName, chatId, disableNotification }: sendContact) {
+    const messageParams = qs.stringify({
+      chat_id: chatId || this.chatId,
+      phone_number: phoneNumber,
+      first_name: firstName,
+      disableNotification: disableNotification || false,
+    })
+    const url = `${this.path}/sendContact?${messageParams}`
+    const response = (await axios(url)).data
+    return response
+  }
+
+  async sendMessage({ message, chatId, disableNotification }: sendMessage) {
+    const messageParams = qs.stringify({
+      chat_id: chatId || this.chatId,
+      text: message,
+      disableNotification: disableNotification || false,
+    })
+    const url = `${this.path}/sendMessage?${messageParams}`
+    const response = (await axios(url)).data
+    return response
+  }
+
+  async sendPoll({ question, options, type, correctOptionID, chatId, disableNotification }: sendPoll) {
+    const messageParams = qs.stringify({
+      chat_id: chatId || this.chatId,
+      question: question,
+      options: JSON.stringify(options),
+      type: type || "regular",
+      correct_option_id: correctOptionID,
+      disableNotification: disableNotification || false,
+    })
+
+    const url = `${this.path}/sendPoll?${messageParams}`
+    const response = (await axios(url)).data
+    return response
+
+  }
+
+  async sendDice(chatId?: string, disableNotification?: boolean) {
+    const messageParams = qs.stringify({
+      chat_id: chatId || this.chatId,
+      disableNotification: disableNotification || false,
+    })
+    const url = `${this.path}/sendDice?${messageParams}`
+    const response = (await axios(url)).data
+    return response
+  }
+
+  async getUpdates() {
+    const response = (await axios(`${this.path}/getUpdates`)).data.result
+    return response
+  }
+
+  async getFirstID() {
+    const response = (await axios(`${this.path}/getUpdates`)).data.result[0].message.from
+    return response
+  }
+}
